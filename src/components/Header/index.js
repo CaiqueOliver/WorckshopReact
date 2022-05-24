78a428add50f0b779bd13da7d3fb32ea0ea398f3
Caique Oliveira <tayko@live.com>
2022-05-23 22:43:29 -0300

    adicionado dark mode theme

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,28 +1,66 @@
-import React, { useContext } from "react";
+import React, { Fragment, useState, useEffect, useContext } from "react";
 import "./style.scss";
 import logoAcessaBr from "../../images/logo-acessaBR.png";
 import logoEbac from "../../images/logo-ebac.png";
-
 import { LocationContext } from "../../contexts/LocationContext";
+
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "../../theme";
+import GlobalTheme from "../../global";
+import styled from "styled-components";
 
 const Header = () => {
   const { city, state } = useContext(LocationContext);
+
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = () => {
+    if (theme === "light") {
+      window.localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    } else {
+      window.localStorage.setItem("theme", "light");
+      setTheme("light");
+    }
+  };
+
+  useEffect(() => {
+    const localTheme = window.localStorage.getItem("theme");
+    localTheme && setTheme(localTheme);
+  }, []);
+
   return (
-    <header className="header__container">
-      <div className=" header__ logo header__logo--state">
-        <img src={logoAcessaBr} alt="Logo AcessaBR" />
-        <span className="header__city">
-          / {city} - {state}
-        </span>
-      </div>
+    <Fragment>
+      <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+        <GlobalTheme />
+      </ThemeProvider>
 
-      <div className="header__ logo header__logo--ebac">
-        <span className="header__span">Apoio:</span>
+      <header className="header__container">
+        <div className=" header__ logo header__logo--state">
+          <img src={logoAcessaBr} alt="Logo AcessaBR" />
+          <span className="header__city">
+            / {city} - {state}
+          </span>
+        </div>
 
-        <img src={logoEbac} alt="Logo Ebac" />
-      </div>
-    </header>
+        <div className="header__ logo header__logo--ebac">
+          <ButtonChange onClick={toggleTheme}>Mudar tema</ButtonChange>
+          <span className="header__span">Apoio:</span>
+
+          <img src={logoEbac} alt="Logo Ebac" />
+        </div>
+      </header>
+    </Fragment>
   );
 };
+
+const ButtonChange = styled.button`
+  background-color: #00b6c1;
+  color: #fff;
+  width: 100px;
+  height: 40px;
+  border-radius: 10px;
+  margin-right: 20px;
+`;
 
 export default Header;
---
+++ src/theme.js
@@ -0,0 +1,11 @@
+export const lightTheme = {
+  body: "#fff",
+  text: "#010101",
+  toggleBorder: "#fff",
+};
+
+export const darkTheme = {
+  body: "#111",
+  text: "#fafafa",
+  toggleBorder: "#6B8096",
+};
